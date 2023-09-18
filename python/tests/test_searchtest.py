33db67a9c47896ea17c4f090c827c2578df18757
Morgan Wajda-Levie <morgan.wajdalevie@gmail.com>
2023-09-18 15:56:50 -0400

    finished preliminary implementation of searchtest

--- python/tests/test_searchtest.py
+++ python/tests/test_searchtest.py
@@ -1,35 +1,16 @@
 """Test the SearchTest object."""
 
-from enum import Enum, auto
-import json
-
 import unittest
+from unittest.mock import patch, call
 
 from twentysolver.grid import Grid
+from twentysolver import searchtest
+from twentysolver.searchtest import MoveType
 
-class MoveType(Enum):
-    """Three types of moves can be made, one by the player, one by the
-    opponent, and one as a result of chance."""
-    PLAYER = auto()
-    OPPONENT = auto()
-    CHANCE = auto()
 
-class TestSearchTestSearch(unittest.TestCase):
-    """Test components of SearchTree."""
-    # Basic design:
-    # 1. Complete DFS search, to specified depth
-    # 2. Each node can be serialized individually, containing just the
-    # (move, grid) tuple for its children.
-    # 3. The resulting structure is a dictionary mapping (move,grid)
-    # tuples to single Node objects.
-    # 4. The first node in the file is the root node.
-
-class TestSearchTestAcceptance(unittest.TestCase):
-    """SearchTest should generate a complete search tree to a specified
-    depth and save the output as json."""
-    def setUp(self):
-        self.early_grid = Grid.from_list([0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 4, 4, 0, 0])
-
+class GridTester:
+    """Parent class containing convenience functions for testing grid validity."""
+    early_grid = [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 4, 4, 0, 0]
     def assert_valid(self, move_type, starting_grid, possible_moves):
         """Asserts that all (move,grid) pairs are valid, meaning that
         the move is possible from starting_grid, and the resulting grid
@@ -46,6 +27,7 @@ class TestSearchTestAcceptance(unittest.TestCase):
         """Asserts that all player (move,grid) pairs are valid."""
         for move, grid in possible_moves:
             with self.subTest(move=move):
+                grid = Grid.from_list([int(t) for t in grid.strip('[]').split(',')])
                 result = starting_grid.validate_move(move)
                 self.assertEqual(grid, result)
 
@@ -53,6 +35,7 @@ class TestSearchTestAcceptance(unittest.TestCase):
         """Asserts that all opponent (move,grid) pairs are valid."""
         for cell, grid in possible_moves:
             with self.subTest(cell=cell):
+                grid = Grid.from_list([int(t) for t in grid.strip('[]').split(',')])
                 self.assertEqual(starting_grid.get_cell_value(*cell), 0)
                 self.assertEqual(grid, starting_grid)
 
@@ -60,6 +43,7 @@ class TestSearchTestAcceptance(unittest.TestCase):
         """Asserts that all chance (move,grid) pairs are valid."""
         for (cell, value), grid in possible_moves:
             with self.subTest(value=value):
+                grid = Grid.from_list([int(t) for t in grid.strip('[]').split(',')])
                 self.assertTrue(value in {2,4})
                 self.assertEqual(grid, starting_grid.insert_tile(cell, value))
 
@@ -98,40 +82,112 @@ class TestSearchTestAcceptance(unittest.TestCase):
             with self.subTest(value=value):
                 self.assertTrue(value in possible_moves)
 
+
+class TestSearchTestSearch(unittest.TestCase, GridTester):
+    """Test components of SearchTree."""
+    def setUp(self):
+        self.early_grid = Grid.from_list(self.early_grid)
+
+    def test_search_max_record(self):
+        """search_max(records, entry, depth) should create a record for the
+        given (move, grid) pair and append it to records, before
+        continuing the search with any children (until depth is exhausted)."""
+        records = [None]
+        entry = searchtest.Entry(None, self.early_grid)
+        searchtest.search_max(records, entry, 1)
+        self.assertEqual(len(records), 2)
+        record = records[-1]
+        self.assertIsInstance(record, dict)
+        self.assertEqual(record,
+                {   'key': (None, self.early_grid.as_list()),
+                    'movetype': 'PLAYER',
+                    'children': [(0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 8]),
+                        (2, [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 8, 0, 0, 0]),
+                        (3, [2, 4, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])],
+                    })
+
+    def test_search_max_calls(self):
+        """search_max(records, entry, depth) should call search_min once
+        for every child, if depth is greater than 0."""
+        entry = searchtest.Entry(None, self.early_grid)
+        with patch.object(searchtest, 'search_min', return_value=None) as search_min:
+            searchtest.search_max([], entry, 1)
+            search_min.assert_not_called()
+        records = []
+        with patch.object(searchtest, 'search_min', return_value=None) as search_min:
+            searchtest.search_max(records, entry, 2)
+            search_min.assert_called()
+            search_min.assert_has_calls(
+                    [call(records, searchtest.Entry(move, grid), 1) for
+                    move, grid in entry.grid.get_available_moves()],
+                    any_order=True)
+
+    def test_search_min_record(self):
+        """search_min(records, entry, depth) should create a record for the
+        given (move, grid) pair and append it to records, before
+        continuing the search with any children (until depth is exhausted)."""
+        grid = self.early_grid.move(0)
+        records = [None]
+        entry = searchtest.Entry(0, grid)
+        searchtest.search_min(records, entry, 1)
+        self.assertEqual(len(records), 2)
+        record = records[-1]
+        self.assertIsInstance(record, dict)
+        self.assertEqual(record,
+                {   'key': (0, grid.as_list()),
+                    'movetype': 'OPPONENT',
+                    'children': [(cell, grid.as_list()) for cell in grid.get_available_cells()],
+                    })
+
+    # Basic design:
+    # 1. Complete DFS search, to specified depth
+    # 2. Each node can be serialized individually, containing just the
+    # (move, grid) tuple for its children.
+    # 3. The resulting structure is a dictionary mapping (move,grid)
+    # tuples to single Node objects.
+    # 4. The first node in the file is the root node.
+
+class TestSearchTestAcceptance(unittest.TestCase, GridTester):
+    """SearchTest should generate a complete search tree to a specified
+    depth and save the output as json."""
+    def setUp(self):
+        self.early_grid = Grid.from_list([0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 4, 4, 0, 0])
+
+
     def test_early_grid_depth1(self):
         """Test an early game grid to a partial depth of 1. (i.e. one
         move, not one turn.)"""
         # initialize grid
         grid = self.early_grid
         # get results of SearchTest
-        search = SearchTest.search(grid, depth=1)
+        json = searchtest.search(grid, depth=1)
         # convert from JSON to Python object
-        nodes, root = SearchTest.from_json(search)
+        records, root = searchtest.from_json(json)
         # assert that all nodes are valid
-        self.assertEqual(grid, root.grid)
-        self.assert_valid(MoveType.PLAYER, grid, root.children)
+        self.assertEqual((None, str(grid.as_list())), root)
+        self.assert_valid(MoveType.PLAYER, grid, records[root].children)
         # assert that no nodes are missing
-        self.assert_complete(MoveType.PLAYER, grid, root.children)
+        self.assert_complete(MoveType.PLAYER, grid, records[root].children)
 
     def test_early_grid_depth3(self):
         """Test an early game grid to a partial depth of 3. (i.e. one
         complete turn.)"""
         grid = self.early_grid
-        tree = SearchTest.search(grid, depth=3)
-        nodes, root = SearchTest.from_json(search)
-        self.assertEqual(grid, root.grid)
+        json = searchtest.search(grid, depth=3)
+        records, root = searchtest.from_json(json)
+        self.assertEqual((None, str(grid.as_list())), root)
         frontier = [root]
+        self.assertIsInstance(root[1], str)
         while frontier:
-            node = frontier.pop()
-            with self.subTest(node=node):
-                self.assert_valid(node.movetype, node.grid, node.children)
-                self.assert_complete(node.movetype, node.grid, node.children)
-                frontier.extend(nodes[c] for c in node.children)
+            record = records[frontier.pop()]
+            with self.subTest(record=record):
+                self.assert_valid(record.movetype, record.grid, record.children)
+                self.assert_complete(record.movetype, record.grid, record.children)
+                if record.movetype != MoveType.CHANCE:
+                    frontier.extend(c for c in record.children)
 
     def test_mid_grid(self):
         """Test a mid-game grid."""
-        self.assertTrue(False)
 
     def test_late_grid(self):
         """Test a late-game grid."""
-        self.assertTrue(False)
